4386dc4b18d4145a24f7b2ab3677f00cf9ccc910
Serhat Yalçınkaya <serhat01yalcinkaya@gmail.com>
2024-08-28 11:19:32 +0300

    Update navigation_node_2.py

--- ROS Nodes/turtlebot_navigation/scripts/navigation_node_2.py
+++ ROS Nodes/turtlebot_navigation/scripts/navigation_node_2.py
@@ -7,6 +7,7 @@ import actionlib
 from geometry_msgs.msg import Pose, Point, Quaternion
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
+from rosgraph_msgs.msg import Log
 
 class TurtleBotNavigator:
 
@@ -20,7 +21,7 @@ class TurtleBotNavigator:
 
         # Önceden belirlenmiş üç hedef noktası
         self.goals = [
-            self.starting_pose,
+        
             Pose(Point(-1.0, 2.0, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)),
             Pose(Point(7.0, 1.0, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)),
             Pose(Point(1.0, 3.0, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)),
@@ -36,10 +37,20 @@ class TurtleBotNavigator:
         # Topic'leri advertise et
         self.start_pub = rospy.Publisher('/start_robot', String, queue_size=10)
         self.stop_pub = rospy.Publisher('/stop_robot', String, queue_size=10)
+        self.status_pub = rospy.Publisher('/robot_status', String, queue_size=10)  # ROS Bridge için yeni publisher
+        self.log_pub = rospy.Publisher('/ros_log_messages', String, queue_size=10) # ROS logları için publisher
 
         # ROS Bridge üzerinden komutları dinlemek için subscriber ekle
         rospy.Subscriber("/start_robot", String, self.ros_command_callback)
         rospy.Subscriber("/stop_robot", String, self.ros_command_callback)
+
+        # Log mesajları için subscriber ekle
+        rospy.Subscriber('/rosout_agg', Log, self.log_callback)
+
+    def log_callback(self, data):
+        # Sadece navigation_node_2.py tarafından üretilen logları yayınla
+        if data.name == rospy.get_name():
+            self.log_pub.publish(data.msg)
 
     def ros_command_callback(self, msg):
         if msg.data == "start":
@@ -71,6 +82,7 @@ class TurtleBotNavigator:
         return result == actionlib.GoalStatus.SUCCEEDED
 
     def start_navigation(self):
+        self.status_pub.publish("Starting navigation...")  # Status mesajı gönder
         rospy.loginfo("Starting navigation...")
         while self.current_goal_index < len(self.goals):
             if not self.is_navigating:
@@ -78,12 +90,14 @@ class TurtleBotNavigator:
             goal = self.goals[self.current_goal_index]
             result = self.move_to_goal(goal)
             if result:
-                rospy.loginfo(f"Reached goal {self.current_goal_index + 1} successfully!")
+                message = f"Reached goal {self.current_goal_index + 1} successfully!"
+                self.status_pub.publish(message)  # Status mesajı gönder
+                rospy.loginfo(message)
                 self.last_goal_index = self.current_goal_index
                 self.current_goal_index += 1
                 self.goal_reached = True
             else:
-                rospy.loginfo("Failed to reach the goal. Retrying...")
+                
                 self.goal_reached = False
 
         self.is_navigating = False
@@ -92,11 +106,12 @@ class TurtleBotNavigator:
         if not self.is_navigating:
             self.is_navigating = True
             if not self.goal_reached and self.current_goal_index == self.last_goal_index:
+                self.status_pub.publish(f"Resuming goal {self.current_goal_index + 1}")  # Status mesajı gönder
                 rospy.loginfo(f"Resuming goal {self.current_goal_index + 1}")
                 self.navigation_thread = threading.Thread(target=self.start_navigation)
                 self.navigation_thread.start()
             else:
-                rospy.loginfo("Starting new navigation sequence.")
+                
                 self.navigation_thread = threading.Thread(target=self.start_navigation)
                 self.navigation_thread.start()
         else:
@@ -108,6 +123,7 @@ class TurtleBotNavigator:
             self.client.cancel_goal()  # Şu anki hedefi iptal eder
             if self.navigation_thread:
                 self.navigation_thread.join()
+            self.status_pub.publish("Navigation stopped.")  # Status mesajı gönder
             rospy.loginfo("Navigation stopped.")
             self.goal_reached = False
         else:
